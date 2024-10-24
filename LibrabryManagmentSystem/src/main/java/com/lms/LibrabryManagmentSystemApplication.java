656b849e82da818bb8317b5cb5a42ade043ef5cf
ShubhamNaikVSH <shubham.naik@vshsolutions.com>
2024-10-25 01:47:02 +0530

    LMS template

---
+++ LibrabryManagmentSystem/src/main/java/com/lms/LibrabryManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrabryManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrabryManagmentSystemApplication.class, args);
+	}
+
+}
